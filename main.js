a589f1a954115fd05f78b1b84944875442a4eeb0
Ryan Graham <ryangrahamwpb@gmail.com>
2020-01-29 16:55:31 -0500

    Add js for addi ng and removing text to screen element.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello there.");
+//Let's add the click event to the buttons
+const buttons = document.querySelectorAll(".btn");
+let screen = document.querySelector(".screen");
+let clear = document.querySelector(".clear");
+for (const button of buttons) {
+  button.addEventListener("click", function() {
+    let value = this.innerHTML;
+    screen.innerHTML += value + " ";
+  });
+}
+
+clear.addEventListener("click", function() {
+  screen.innerHTML = " ";
+});
