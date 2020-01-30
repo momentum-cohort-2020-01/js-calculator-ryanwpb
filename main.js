3af32da754738b69e98e9bbfc98c5a6eb4989cf1
Ryan Graham <ryangrahamwpb@gmail.com>
2020-01-29 22:09:44 -0500

    Add calculation functionality.

--- main.js
+++ main.js
@@ -3,15 +3,25 @@ const buttons = document.querySelectorAll(".btn");
 let screen = document.querySelector(".screen");
 let clear = document.querySelector(".clear");
 let equal = document.querySelector("#equal");
+
+// let evaluation = currentCalc.innerText;
+// let answer = eval(evaluation);
+// currentCalc.innerHTML=answer;
+
 for (const button of buttons) {
   button.addEventListener("click", function() {
     let value = this.innerHTML;
-    screen.innerHTML += value + " ";
+    screen.innerText += value + " ";
   });
 }
 
 clear.addEventListener("click", function() {
-  screen.innerHTML = " ";
+  screen.innerText = " ";
 });
 
-equal.addEventListener("click", function() {});
+equal.addEventListener("click", function(val) {
+  let result = screen.innerText;
+  let answer = eval(result);
+  console.log(answer);
+  screen.innerText = answer;
+});
